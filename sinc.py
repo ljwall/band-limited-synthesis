db0358b01873bd2b6738657b27cfdbfbad6270cb
liam <liam.wall@gmail.com>
2021-06-17 21:57:42 +0100

    Function for np array for windowed bandlimited impulse

--- sinc.py
+++ sinc.py
@@ -32,6 +32,23 @@ def bl_impulse(fs, t):
     """
     return sinc(np.pi * fs * t)
 
+def windowed_bl_impulse(fs, over_sampling, crossings):
+    """
+    Creates a np array for a windowed band-limited impulse function
+
+    args:
+        fs - audio sample rate
+        over_sampling - factor to oversample this function
+        crossings - how many zero crossings should be included in the window (on each side of x=0)
+
+    returns:
+        numpy array
+    """
+    times = np.linspace(-crossings/fs, crossings/fs, 2*crossings*over_sampling)
+    window = signal.windows.blackman(2*crossings*over_sampling)
+    fn = np.vectorize(lambda t: bl_impulse(fs, t))
+    return window * fn(times)
+
 if __name__ == '__main__':
     N = 40000
     x = np.linspace(-60, 60, N)
@@ -39,4 +56,6 @@ if __name__ == '__main__':
     bw  = signal.windows.blackman(N)
     plt.plot(x, y)
     plt.plot(x, y*bw)
+    plt.show()
+    plt.plot(windowed_bl_impulse(44100, 1024, 16))
     plt.show()
